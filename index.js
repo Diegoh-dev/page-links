7032fae6e208fdfc78b19e52db96be8039eb424a
diego costa <diegoh.developer@gmail.com>
2022-05-13 03:14:55 -0300

    mensagem de alerta no botão portfolio

---
+++ index.js
@@ -0,0 +1,6 @@
+const portifolio = document.querySelector("#portifolio");
+
+const alerta = () => {
+  alert("Estamos desenvolvendo o portfolio em breve ele estará disponível.");
+};
+portifolio.addEventListener("click", alerta);
